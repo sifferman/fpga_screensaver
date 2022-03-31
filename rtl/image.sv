ac9bfe87bc5916270cd38372452d522f55bc8570
E4tHam <ethan@sifferman.dev>
2022-03-30 21:29:25 -0700

    fixed image.sv definition

--- rtl/image.sv
+++ rtl/image.sv
@@ -2,9 +2,9 @@
 module image #(
     parameter SELECT = 0
 ) (
-    input               clk,
-    input logic   [9:0] position_x, position_x_NEXT,
-    input logic   [8:0] position_y, position_y_NEXT,
+    input               clk, rst,
+    input         [9:0] position_x, position_x_NEXT,
+    input         [8:0] position_y, position_y_NEXT,
     input        [31:0] frame,
     output logic  [3:0] r, g, b
 );
