275601b61019cc932924b22e3de9f159a296daf0
E4tHam <ethan@sifferman.dev>
2022-04-30 13:35:03 -0700

    updated image.sv to match workshop

--- rtl/image.sv
+++ rtl/image.sv
@@ -1,12 +1,14 @@
 
 module image #(
-    parameter SELECT = 0
+    parameter SELECT        = 0,
+    parameter SCREEN_WIDTH  = 640,
+    parameter SCREEN_HEIGHT = 480
 ) (
-    input               clk, rst,
-    input         [9:0] position_x, position_x_NEXT,
-    input         [8:0] position_y, position_y_NEXT,
-    input        [31:0] frame,
-    output logic  [3:0] r, g, b
+    input                               clk, rst,
+    input    [$clog2(SCREEN_WIDTH)-1:0] position_x, position_x_next,
+    input   [$clog2(SCREEN_HEIGHT)-1:0] position_y, position_y_next,
+    input                        [31:0] frame,
+    output logic                  [3:0] r, g, b
 );
 
 generate
@@ -14,7 +16,7 @@ generate
 if (SELECT == 0) begin : checkerboard
 //
 
-wire color_next = position_x_NEXT[6] ^ position_y_NEXT[6];
+wire color_next = position_x_next[6] ^ position_y_next[6];
 
 always_ff @ (posedge clk) begin
     r <= {4{color_next&frame[1]}};
@@ -28,28 +30,28 @@ end
 if (SELECT == 1) begin : fractal
 //
 
-wire valid_NEXT = (10'd64 <= position_x_NEXT) && (position_x_NEXT < 10'd576);
+wire valid_next = (10'd64 <= position_x_next) && (position_x_next < 10'd576);
 
-wire [8:0] shifted_x_NEXT = valid_NEXT ? 9'(position_x_NEXT - 10'd64) : {9{1'bx}};
-wire [8:0] shifted_y_NEXT = valid_NEXT ? 9'(position_y_NEXT +  9'd16) : {9{1'bx}};
+wire [8:0] shifted_x_next = valid_next ? 9'(position_x_next - 10'd64) : {9{1'bx}};
+wire [8:0] shifted_y_next = valid_next ? 9'(position_y_next +  9'd16) : {9{1'bx}};
 
-logic color_NEXT;
-logic [3:0] r_NEXT, g_NEXT, b_NEXT;
+logic color_next;
+logic [3:0] r_next, g_next, b_next;
 integer i;
 always_comb begin
-    color_NEXT = 1'b1;
+    color_next = 1'b1;
     for (i = 8; i >= 1; i=i-2) begin
-        color_NEXT = color_NEXT & ((shifted_x_NEXT[i]!=shifted_x_NEXT[i-1]) || (shifted_y_NEXT[i]!=shifted_y_NEXT[i-1]));
+        color_next = color_next & ((shifted_x_next[i]!=shifted_x_next[i-1]) || (shifted_y_next[i]!=shifted_y_next[i-1]));
     end
-    r_NEXT = valid_NEXT?{4{color_NEXT}}:4'b0;
-    g_NEXT = valid_NEXT?{4{color_NEXT}}:4'b0;
-    b_NEXT = valid_NEXT?{4{color_NEXT}}:4'b0;
+    r_next = valid_next?{4{color_next}}:4'b0;
+    g_next = valid_next?{4{color_next}}:4'b0;
+    b_next = valid_next?{4{color_next}}:4'b0;
 end
 
 always_ff @ (posedge clk) begin
-    r <= r_NEXT;
-    g <= g_NEXT;
-    b <= b_NEXT;
+    r <= r_next;
+    g <= g_next;
+    b <= b_next;
 end
 
 //
